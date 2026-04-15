369e81de98ab5b24f6bc9b8c1c6cf502e2cd3ea8
Lavanya R <lavanyalavu292005@gmail.com>
2026-04-15 16:42:07 +0530

    Add files via upload

---
+++ ExampleForOperators.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class ExampleForOperators {
+
+	public static void main(String[] args) {
+		
+		System.out.println("---------Arithmetic Operators-------");
+		
+		int num1=30;
+		int num2=40;
+		System.out.println("Addition" + (num1+num2));
+		System.out.println("Subtraction" + (num1-num2));
+		System.out.println("Multiplication" + (num1*num2));
+		System.out.println("Division" + (num1/num2));
+		 
+	}
+
+}
---
+++ FirstProgram.java
@@ -0,0 +1,18 @@
+package Day2;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 20;
+		float height = 4.5f;
+		//double h = 6.1; single line comment
+		System.out.println("The age of the student " + age);
+		System.out.println("The height of the student " + height);
+		/*-------------------------------------------------------
+		 * ---multi line comment---------------------------------
+		 */
+	}
+
+}
