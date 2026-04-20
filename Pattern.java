9afb0314e345bf8e0d86937a3dea9dac409e9325
Lavanya R <lavanyalavu292005@gmail.com>
2026-04-20 12:25:41 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,36 @@
+package day6;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		
+		for (int i=1; i <=4; i++)
+		{
+			for (int j=1; j<=i ; j++)
+		{		
+			System.out.print("*"+" ");
+		}
+			System.out.println( );
+		}
+		
+		/*----pattern2---*/
+		int n = 5;  
+        
+        for (int i = 1; i <= n; i++) 
+        {
+            for (int j = 1; j <= n; j++) 
+            {
+            	if (i == 1 || i == n || j == 1 || j == n) 
+                {
+                    System.out.print("*");
+                } 
+                else 
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+	}
+	}
+	
+}
